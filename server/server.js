c360c2cbc187473831ba8090ea67ea8165abff28
Divyansh Bhatia <divyanshbhatia@Divyanshs-MacBook-Pro.local>
2018-03-25 23:44:12 -0700

    feat: introducing sockets

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname,'../public');
 
 var express = require('express');
+
+
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
+io.on('connection',(socket) => {
+  console.log('New user connected');
 
-app.listen(port, () => {
+  socket.on('disconnect',() =>{
+      console.log('user disconnected');
+  });
+});
+server.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
 
